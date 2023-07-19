68cc901bf9e2fca92e169cdac133c2eea6a51688
balajipatil0009 <balajipatil20030@gmail.com>
2023-07-19 15:47:17 +0530

    netlify

--- src/modules/axios.jsx
+++ src/modules/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create(
     {
-        baseURL:'http://localhost:3000',
+        baseURL:'https://instapost-server.onrender.com', //http://localhost:3000
         headers:{
             "Content-Type":"application/json"
           },
